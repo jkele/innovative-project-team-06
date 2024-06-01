17c5da1d558c584286ea77aaec2b568979db5929
jkele <josip.skrill@gmail.com>
2024-06-01 14:31:27 +0200

    Add project card component

--- src/components/project-card/components/ProjectCard.tsx
+++ src/components/project-card/components/ProjectCard.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/utils/cn";
 import { ProjectCardProperties } from "../types/project-card";
 
 export const ProjectCard = (properties: ProjectCardProperties) => {
@@ -8,8 +9,13 @@ export const ProjectCard = (properties: ProjectCardProperties) => {
   };
 
   return (
-    <div className="flex flex-row">
-      <div className="flex flex-col px-2 py-3 gap-1 bg-white">
+    <div className="flex flex-row shadow-lg">
+      <div
+        className={cn(
+          "flex flex-col px-2 py-3 gap-1 ",
+          project.finished ? "bg-[#F3FAFF]" : "bg-white"
+        )}
+      >
         <p className="font-medium">{project.title}</p>
         <p className="text-[#054166] pl-4">
           {shortenDescriptionText(project.description)}
