f2fd553ba9f4e9baf0d2b032be2aad6e9c8dea60
jkele <josip.skrill@gmail.com>
2024-06-01 15:25:13 +0200

    Add project info page

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -8,6 +8,35 @@ export const PROJECTS = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, ultricies nunc. Nulla facilisi. Nullam nec nunc nec nunc.",
     finished: false,
     street: "Antunovac 8B",
+    punctures: [
+      {
+        id: "1",
+        title: "Puncture 1",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, ultricies nunc. Nulla facilisi. Nullam nec nunc nec nunc.",
+        finished: false,
+        width: "30",
+        height: "78",
+      },
+      {
+        id: "2",
+        title: "Puncture 2",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, ultricies nunc. Nulla facilisi. Nullam nec nunc nec nunc.",
+        finished: false,
+        width: "28",
+        height: "30",
+      },
+      {
+        id: "3",
+        title: "Puncture 3",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, ultricies nunc. Nulla facilisi. Nullam nec nunc nec nunc.",
+        finished: true,
+        width: "28",
+        height: "30",
+      },
+    ],
   },
   {
     id: "2",
@@ -16,6 +45,26 @@ export const PROJECTS = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, ultricies nunc. Nulla facilisi. Nullam nec nunc nec nunc.",
     finished: false,
     street: "Heinzlova 22",
+    punctures: [
+      {
+        id: "1",
+        title: "Puncture 1",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, ultricies nunc. Nulla facilisi. Nullam nec nunc nec nunc.",
+        finished: false,
+        width: "30",
+        height: "78",
+      },
+      {
+        id: "2",
+        title: "Puncture 2",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, ultricies nunc. Nulla facilisi. Nullam nec nunc nec nunc.",
+        finished: false,
+        width: "28",
+        height: "30",
+      },
+    ],
   },
   {
     id: "3",
@@ -24,6 +73,26 @@ export const PROJECTS = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, ultricies nunc. Nulla facilisi. Nullam nec nunc nec nunc.",
     finished: false,
     street: "Kneza Branimira 29",
+    punctures: [
+      {
+        id: "1",
+        title: "Puncture 1",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, ultricies nunc. Nulla facilisi. Nullam nec nunc nec nunc.",
+        finished: false,
+        width: "30",
+        height: "78",
+      },
+      {
+        id: "2",
+        title: "Puncture 2",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, ultricies nunc. Nulla facilisi. Nullam nec nunc nec nunc.",
+        finished: false,
+        width: "28",
+        height: "30",
+      },
+    ],
   },
   {
     id: "4",
@@ -32,5 +101,25 @@ export const PROJECTS = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, ultricies nunc. Nulla facilisi. Nullam nec nunc nec nunc.",
     finished: true,
     street: "Gradišćanska 24",
+    punctures: [
+      {
+        id: "1",
+        title: "Puncture 1",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, ultricies nunc. Nulla facilisi. Nullam nec nunc nec nunc.",
+        finished: false,
+        width: "30",
+        height: "78",
+      },
+      {
+        id: "2",
+        title: "Puncture 2",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, ultricies nunc. Nulla facilisi. Nullam nec nunc nec nunc.",
+        finished: false,
+        width: "28",
+        height: "30",
+      },
+    ],
   },
 ] as Project[];
