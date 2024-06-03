f1495642c173f27a9ea3aa322aaed272d81b9816
jkele <josip.skrill@gmail.com>
2024-06-03 20:16:20 +0200

    Fix dashboard desktop style

--- src/features/projects-list/components/ProjectsList.tsx
+++ src/features/projects-list/components/ProjectsList.tsx
@@ -10,14 +10,25 @@ export const ProjectsList = (properties: ProjectsListProperties) => {
 
   return (
     <div className="flex flex-col gap-4">
-      <p className="text-[#5F8DAA] text-center">Projects</p>
-      {unfinishedProjects.map((project) => (
-        <ProjectCard project={project} key={project.id} />
-      ))}
-      <p className="text-[#5F8DAA] text-center mt-10">Finished projects</p>
-      {finishedProjects.map((project) => (
-        <ProjectCard project={project} key={project.id} />
-      ))}
+      <p className="text-[#5F8DAA] text-center lg:text-3xl font-semibold mt-10">
+        Projects
+      </p>
+      <div className="flex flex-col gap-4 lg:flex-row lg:flex-wrap mt-10">
+        {unfinishedProjects.map((project) => (
+          <ProjectCard project={project} key={project.id} />
+        ))}
+      </div>
+      <button className="lg:mt-16 mt-4 bg-white uppercase font-semibold shadow-lg py-4">
+        Add project
+      </button>
+      <p className="text-[#5F8DAA] text-center lg:text-3xl font-semibold mt-10">
+        Finished projects
+      </p>
+      <div className="flex flex-col gap-4 lg:flex-row lg:flex-wrap mt-10">
+        {finishedProjects.map((project) => (
+          <ProjectCard project={project} key={project.id} />
+        ))}
+      </div>
     </div>
   );
 };
